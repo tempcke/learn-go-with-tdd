2316628fbf23dad339a4cd65f04700ba0bdb2a83
tempcke <t.github@logikostech.net>
2019-05-24 13:38:12 -0500

    Shape interface

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestPerimeter(t *testing.T) {
-	assertPerimeter := func(expected float64, actual float64) {
+	assertPerimeter := func(t *testing.T, expected float64, actual float64) {
 		t.Helper()
 
 		if actual != expected {
@@ -18,30 +18,29 @@ func TestPerimeter(t *testing.T) {
 		r1 := Rectangle{10.0, 10.0}
 		r2 := Rectangle{10.0, 20.0}
 
-		assertPerimeter(40.0, r1.Perimeter())
-		assertPerimeter(60.0, r2.Perimeter())
+		assertPerimeter(t, 40.0, r1.Perimeter())
+		assertPerimeter(t, 60.0, r2.Perimeter())
 	})
 }
 
 func TestArea(t *testing.T) {
-	assertArea := func(expected float64, actual float64) {
-		t.Helper()
-
-		if actual != expected {
-			t.Errorf("got %.2f want %.2f", actual, expected)
-		}
-	}
-
 	t.Run("rectangles", func(t *testing.T) {
 
 		r1 := Rectangle{2.0,3.0}
 		r2 := Rectangle{3.0,4.0}
 
-		assertArea(6.0, r1.Area())
-		assertArea(12.0, r2.Area())
+		assertShapeArea(t,6.0, r1)
+		assertShapeArea(t,12.0, r2)
 	})
 
 	t.Run("circles", func(t *testing.T) {
-		assertArea(314.1592653589793, Circle{10}.Area())
+		assertShapeArea(t, 314.1592653589793, Circle{10})
 	})
+}
+
+func assertShapeArea(t *testing.T, expected float64, shape Shape) {
+	actual := shape.Area()
+	if actual != expected {
+		t.Errorf("got %.2f want %.2f", actual, expected)
+	}
 }
