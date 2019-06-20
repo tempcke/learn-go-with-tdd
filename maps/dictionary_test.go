7a8a3e76832c99b6014324cab29bd7ffb0d6dff4
tempcke <t.github@logikostech.net>
2019-06-20 15:32:03 -0500

    started dictionary

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,5 +3,18 @@ package main
 import "testing"
 
 func TestSearch(t *testing.T) {
-	t.Error("https://github.com/quii/learn-go-with-tests/blob/master/maps.md")
+	dictionary := map[string]string{"test": "this is just a test"}
+
+	assertEquals(t, "this is just a test", Search(dictionary, "test"))
+}
+
+func assertEquals(t *testing.T, expected string, actual string) {
+	t.Helper()
+
+	if actual != expected {
+		t.Errorf(
+			"Strings are not the same\nexpected\t'%s'\nreceived\t'%s'",
+			expected,
+			actual)
+	}
 }
