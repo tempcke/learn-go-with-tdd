5fbd888653dbfb1b90a8015ae959dbf3a3a1994a
tempcke <t.github@logikostech.net>
2019-06-20 15:33:10 -0500

    left off

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -6,7 +6,10 @@ func TestSearch(t *testing.T) {
 	dictionary := map[string]string{"test": "this is just a test"}
 
 	assertEquals(t, "this is just a test", Search(dictionary, "test"))
+
+	t.Error("Left off here: https://github.com/quii/learn-go-with-tests/blob/master/maps.md#using-a-custom-type")
 }
+
 
 func assertEquals(t *testing.T, expected string, actual string) {
 	t.Helper()
