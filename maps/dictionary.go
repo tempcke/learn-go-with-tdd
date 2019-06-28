9b4ade43515438b4796c35fd028cef662f46e98d
tempcke <t.github@logikostech.net>
2019-06-28 15:12:57 -0500

    dictionary search, add, has, update all implemented.  next up: delete

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,5 +1,49 @@
 package main
 
-func Search(dictionary map[string]string, word string) string {
-	return dictionary[word]
+type Dictionary map[string]string
+
+type DictionaryErr string
+
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
+)
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+func (d Dictionary) Search(word string) (string, error) {
+	if !d.Has(word) {
+		return "", ErrNotFound
+	}
+
+	return d[word], nil
+}
+
+func (d Dictionary) Add(word string, definition string) error {
+	if d.Has(word) {
+		return ErrWordExists
+	}
+
+	d[word] = definition
+
+	return nil
+}
+
+func (d Dictionary) Update(word string, definition string) error {
+	if !d.Has(word) {
+		return ErrWordDoesNotExist
+	}
+
+	d[word] = definition
+
+	return nil
+}
+
+func (d Dictionary) Has(word string) bool {
+	_, ok := d[word]
+
+	return ok
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -1,23 +1,122 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
+	d := Dictionary{"test": "this is just a test"}
 
-	assertEquals(t, "this is just a test", Search(dictionary, "test"))
+	t.Run("can get definition of known word", func(t *testing.T) {
+		def, _ := d.Search("test")
+		assertEquals(t, "this is just a test", def)
+	})
 
-	t.Error("Left off here: https://github.com/quii/learn-go-with-tests/blob/master/maps.md#using-a-custom-type")
+	t.Run("expect error when word undefined", func(t *testing.T) {
+		word := "foo"
+		def, err := d.Search(word)
+
+		assertEquals(t, "", def)
+		assertError(t, ErrNotFound, err)
+	})
 }
 
+func TestAdd(t *testing.T) {
+	d := Dictionary{}
+
+	word := "test"
+	def  := "this is just a test"
+
+	t.Run("new word", func(t *testing.T) {
+		err := d.Add(word, def)
+
+		assertErrorIsNil(t, err)
+		assertDefinition(t, d, word, def)
+	})
+
+	t.Run("existing word", func(t * testing.T) {
+		_ = d.Add(word, def)
+
+		err := d.Add(word, "something else")
+
+		assertError(t, ErrWordExists, err)
+		assertDefinition(t, d, word, def)
+	})
+}
+
+func TestUpdate(t *testing.T) {
+	d := Dictionary{}
+
+	word   := "test"
+	def    := "this is just a test"
+	newDef := "this is just a test, only a test"
+
+	t.Run("update new word should fail", func(t *testing.T) {
+		updateError := d.Update(word, def)
+
+		_, searchError := d.Search(word)
+
+		assertError(t, ErrWordDoesNotExist, updateError)
+		assertError(t, ErrNotFound, searchError)
+	})
+
+	t.Run("Update existing word", func(t *testing.T) {
+		_ = d.Add(word, def)
+		err := d.Update(word, newDef)
+
+		assertErrorIsNil(t, err)
+		assertDefinition(t, d, word, newDef)
+	})
+}
+
+func TestHas(t *testing.T) {
+	goodWord := "test"
+	badWord  := "abcdefg"
+
+	d := Dictionary{}
+
+	_ = d.Add(goodWord, "this is a test")
+
+	if !d.Has(goodWord) {
+		t.Errorf("d.Has(%s) should be true but was false", goodWord)
+	}
+	if d.Has(badWord) {
+		t.Errorf("d.Has(%s) should be false but was true", badWord)
+	}
+}
+
+func TestDelete(t *testing.T) {
+	t.Error("https://github.com/quii/learn-go-with-tests/blob/master/maps.md#write-the-test-first-6");
+}
 
 func assertEquals(t *testing.T, expected string, actual string) {
 	t.Helper()
 
 	if actual != expected {
-		t.Errorf(
-			"Strings are not the same\nexpected\t'%s'\nreceived\t'%s'",
-			expected,
-			actual)
+		message := "Strings are not the same\n" +
+			"expected\t'%s'\n" +
+			"received\t'%s'"
+
+		t.Errorf(message,	expected,	actual)
 	}
+}
+
+func assertError(t *testing.T, expected, received error) {
+	t.Helper()
+	assertEquals(t, expected.Error(), received.Error())
+}
+
+func assertErrorIsNil(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatal("Unexpected error:", err)
+	}
+}
+
+func assertDefinition(t *testing.T, d Dictionary, word, expected string) {
+	t.Helper()
+	actual, err := d.Search(word)
+
+	assertErrorIsNil(t, err)
+	assertEquals(t, expected, actual)
 }
