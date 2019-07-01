370c49d79d1ad633adaa06593eb58a513a39bd0c
tempcke <t.github@logikostech.net>
2019-07-01 15:40:31 -0500

    finishing up di

--- di/di_test.go
+++ di/di_test.go
@@ -1,7 +1,18 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"gotest.tools/assert"
+	"testing"
+)
 
-func TestDi(t *testing.T) {
-	t.Error("https://github.com/quii/learn-go-with-tests/blob/master/dependency-injection.md")
+//func TestDi(t *testing.T) {
+//	t.Error("https://github.com/quii/learn-go-with-tests/blob/master/dependency-injection.md")
+//}
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Chris")
+
+	assert.Equal(t, "hello, Chris", buffer.String())
 }
