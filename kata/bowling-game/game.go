6acf2f294a6a4f8c87469261580041fc2774621b
tempcke <t.bitbucket@logikostech.net>
2019-09-08 19:09:48 -0500

    roll all 1's and get 20 points

--- kata/bowling-game/game.go
+++ kata/bowling-game/game.go
@@ -5,10 +5,16 @@ type Game struct {
 	roles []int
 }
 
-func (g Game) Roll(pins int) {
+func (g *Game) Roll(pins int) {
 	g.roles = append(g.roles, pins)
 }
 
-func (g Game) Score() int {
-	return 0
+func (g *Game) Score() int {
+	sum := 0
+
+	for i := range g.roles {
+		sum += g.roles[i]
+    }
+
+	return sum
 }
--- kata/bowling-game/game_test.go
+++ kata/bowling-game/game_test.go
@@ -5,10 +5,27 @@ import (
 	"testing"
 )
 
+// RULES
+// Gutter game scores zero - When you roll all misses, you get a total score of zero.
+// All ones scores 20 - When you knock down one pin with each ball, your total score is 20.
+// A spare in the first frame, followed by three pins, followed by all misses scores 16.
+// A strike in the first frame, followed by three and then four pins, followed by all misses, scores 24.
+// A perfect game (12 strikes) scores 300.
+
 func TestGutterGame(t *testing.T) {
-  g := Game{}
-  for i:=0; i<20; i++ {
-  	g.Roll(0)
-  }
-  assert.Equal(t, 0, g.Score())
+	g := Game{}
+	role(&g, 0, 20)
+	assert.Equal(t, 0, g.Score())
+}
+
+func TestAllOnes(t *testing.T) {
+	g := Game{}
+	role(&g, 1, 20)
+	assert.Equal(t, 20, g.Score())
+}
+
+func role(g *Game, pins int, roles int) {
+	for i := 0; i < roles; i++ {
+		g.Roll(pins)
+	}
 }
