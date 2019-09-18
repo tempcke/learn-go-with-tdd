fb46a55491859a0b97f9af04fc56359218649531
tempcke <t.github@logikostech.net>
2019-09-18 11:44:10 -0500

    2. A single number returns the value

--- kata/string-calculator/stringcalc.go
+++ kata/string-calculator/stringcalc.go
@@ -1,5 +1,18 @@
 package string_calculator
 
+import "strconv"
+
 func resolve(input string) int {
+	if n, err := str2int(input); err == nil {
+		return n
+	}
 	return 0
+}
+
+func str2int(input string) (i int, err error) {
+	n, err := strconv.ParseInt(input, 10, 64)
+	if err != nil {
+		return 0, err
+	}
+	return int(n), nil
 }
--- kata/string-calculator/stringcalc_test.go
+++ kata/string-calculator/stringcalc_test.go
@@ -1,12 +1,39 @@
 package string_calculator
 
 import (
-	"gotest.tools/assert"
 	"testing"
 )
 
-func TestEmptyStringReturnsZero(t *testing.T) {
-	input := ""
-	result := resolve(input)
-	assert.Equal(t, 0, result)
+type requirement struct {
+	desc     string
+	input    string
+	expected int
+}
+
+var requirements = []requirement{
+	{
+		"An empty string returns zero",
+		"",
+		0,
+	},
+	{
+		"A single number returns the value",
+		"42",
+		42,
+	},
+}
+
+func TestStringCalculator(t *testing.T) {
+	for _, req := range requirements {
+		t.Run(req.desc, func(t *testing.T) {
+			checkRequirement(t, req)
+		})
+	}
+}
+
+func checkRequirement(t *testing.T, req requirement) {
+	result := resolve(req.input)
+	if req.expected != result {
+		t.Errorf("\nWant  %d\nGot   %d\n", req.expected, result)
+	}
 }
