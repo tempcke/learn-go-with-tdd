4ee343fecd6a572a4f2daabe34b3590ae8c66b83
tempcke <t.bitbucket@logikostech.net>
2019-10-12 06:50:19 -0500

    kata - num2lcd - attempted cleanup

--- kata/num2lcd/num2lcd.go
+++ kata/num2lcd/num2lcd.go
@@ -9,59 +9,50 @@ func Num2Lcd(num int) string {
 
 type digit [3]string
 
-func numberSegments(n int) digit {
-	switch n {
-	case 0: return digit{
+var digits = [...]digit{
+	{
 		" _ ",
 		"| |",
 		"|_|",
-	}
-	case 1: return digit{
+	}, {
 		"   ",
 		"  |",
 		"  |",
-	}
-	case 2: return digit{
+	}, {
 		" _ ",
 		" _|",
 		"|_ ",
-	}
-	case 3: return digit{
+	}, {
 		" _ ",
 		" _|",
 		" _|",
-	}
-	case 4: return digit{
+	},{
 		"   ",
 		"|_|",
 		"  |",
-	}
-	case 5: return digit{
+	},{
 		" _ ",
 		"|_ ",
 		" _|",
-	}
-	case 6: return digit{
+	},{
 		" _ ",
 		"|_ ",
 		"|_|",
-	}
-	case 7: return digit{
+	},{
 		" _ ",
 		"  |",
 		"  |",
-	}
-	case 8: return digit{
+	},{
 		" _ ",
 		"|_|",
 		"|_|",
-	}
-	case 9: return digit{
+	},{
 		" _ ",
 		"|_|",
 		" _|",
-	}
-	default: return digit{}
-	}
+	},
 }
 
+func numberSegments(n int) digit {
+	return digits[n]
+}
