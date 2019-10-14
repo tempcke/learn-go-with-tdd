43c2dcf44cde25b990d90b7fc2ed59077a627437
tempcke <t.github@logikostech.net>
2019-10-14 13:52:53 -0500

    learn-go-with-tdd - refactor - cleanup tests

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -5,70 +5,70 @@ import (
 	"testing"
 )
 
+type requirement struct {
+	Name     string
+	Input    interface{}
+	Expected []string
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+var requirements = []requirement{
+	{
+		"Struct with one string field",
+		struct {
+			Name string
+		}{"Chris"},
+		[]string{"Chris"},
+	}, {
+		"Struct with two string fields",
+		struct {
+			Name string
+			City string
+		}{"Chris", "London"},
+		[]string{"Chris", "London"},
+	}, {
+		"Struct with non string field",
+		struct {
+			Name string
+			Age  int
+		}{"Chris", 33},
+		[]string{"Chris"},
+	}, {
+		"Nested fields",
+		Person{
+			"Chris",
+			Profile{33, "London"},
+		},
+		[]string{"Chris", "London"},
+	}, {
+		"Pointers to things",
+		&Person{
+			"Chris",
+			Profile{33, "London"},
+		},
+		[]string{"Chris", "London"},
+	}, {
+		"Slices",
+		[]Profile{
+			{33, "London"},
+			{34, "Chicago"},
+		},
+		[]string{"London", "Chicago"},
+	},
+}
+
 func TestWalk(t *testing.T) {
 
-	type requirement struct {
-		Name     string
-		Input    interface{}
-		Expected []string
-	}
-
-	type Profile struct {
-		Age  int
-		City string
-	}
-
-	type Person struct {
-		Name    string
-		Profile Profile
-	}
-
-	cases := []requirement{
-		{
-			"Struct with one string field",
-			struct {
-				Name string
-			}{"Chris"},
-			[]string{"Chris"},
-		}, {
-			"Struct with two string fields",
-			struct {
-				Name string
-				City string
-			}{"Chris", "London"},
-			[]string{"Chris", "London"},
-		}, {
-			"Struct with non string field",
-			struct {
-				Name string
-				Age  int
-			}{"Chris", 33},
-			[]string{"Chris"},
-		}, {
-			"Nested fields",
-			Person{
-				"Chris",
-				Profile{33, "London"},
-			},
-			[]string{"Chris", "London"},
-		}, {
-			"Pointers to things",
-			&Person{
-				"Chris",
-				Profile{33, "London"},
-			},
-			[]string{"Chris", "London"},
-		}, {
-			"Slices",
-			[]Profile {
-				{33, "London"},
-				{34, "Chicago"},
-			},
-			[]string{"London", "Chicago"},
-		},
-	}
-
-	for _, test := range cases {
+	for _, test := range requirements {
 		t.Run(test.Name, func(t *testing.T) {
 			var got []string
 			walk(test.Input, func(input string) {
