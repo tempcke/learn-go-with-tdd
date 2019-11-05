7e8cf4ae354c7e15b6c72e6791c6098d8c7e42ae
tempcke <t.github@logikostech.net>
2019-11-05 12:05:45 -0600

    convert test to use a map, just because

--- roman-numerals/numerals_test.go
+++ roman-numerals/numerals_test.go
@@ -6,53 +6,49 @@ import (
 	"testing"
 )
 
-type req struct {
-	i int     // number
-	r string  // expected roman
+var IntRomMap = map[int]string {
+	1: "I",
+	2: "II",
+	3: "III",
+	4: "IV",
+	5: "V",
+	6: "VI",
+	7: "VII",
+	8: "VIII",
+	9: "IX",
+	10: "X",
+	11: "XI",
+	12: "XII",
+	14: "XIV",
+	15: "XV",
+	18: "XVIII",
+	19: "XIX",
+	20: "XX",
+	38: "XXXVIII",
+	39: "XXXIX",
+	40: "XL",
+	47: "XLVII",
+	49: "XLIX",
+	50: "L",
+	90: "XC",
+	100: "C",
+	400: "CD",
+	500: "D",
+	900: "CM",
+	1000: "M",
+	1984: "MCMLXXXIV",
+	3999: "MMMCMXCIX",
+	2014: "MMXIV",
+	1006: "MVI",
+	798: "DCCXCVIII",
 }
 
-var reqs = []req{
-	{1, "I"},
-	{2, "II"},
-	{3, "III"},
-	{4, "IV"},
-	{5, "V"},
-	{6, "VI"},
-	{7, "VII"},
-	{8, "VIII"},
-	{9,"IX"},
-	{10, "X"},
-	{11,"XI"},
-	{12,"XII"},
-	{14,"XIV"},
-	{15,"XV"},
-	{18,"XVIII"},
-	{19,"XIX"},
-	{20,"XX"},
-	{38,"XXXVIII"},
-	{39,"XXXIX"},
-	{40,"XL"},
-	{47,"XLVII"},
-	{49,"XLIX"},
-	{50,"L"},
-	{90,"XC"},
-	{100,"C"},
-	{400,"CD"},
-	{500,"D"},
-	{900,"CM"},
-	{1000,"M"},
-	{1984,"MCMLXXXIV"},
-	{3999,"MMMCMXCIX"},
-	{2014,"MMXIV"},
-	{1006,"MVI"},
-	{798,"DCCXCVIII"},
-}
 
-func TestRomanNumerals(t *testing.T) {
-	for _, req := range reqs {
-		testName := fmt.Sprintf("Test %d is %s", req.i, req.r)
+func TestConvertToRoman(t *testing.T) {
+	for n, r := range IntRomMap {
+		testName := fmt.Sprintf("Test %d is %s", n, r)
 		t.Run(testName, func(t *testing.T) {
-			assertRoman(t, req.i, req.r)
+			assertRoman(t, n, r)
 		})
 	}
 }
@@ -64,3 +60,4 @@ func assertRoman(t *testing.T, n int, expected string) {
 		t, expected, r,
 		msg, n, expected, r)
 }
+
